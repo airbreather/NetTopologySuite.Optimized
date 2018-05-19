60c2f09cf587d7031449b13ac30368864d1886ca
Joe Amenta <airbreather@linux.com>
2018-05-19 09:08:26 -0400

    Add an optimized WKT writer implementation. It's not quite as optimized as I'd like, but I don't see a zero-allocation formatter for double right now.

---
+++ NetTopologySuite.Optimized.Tests/WKTTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using GeoAPI.Geometries;
+using NetTopologySuite.IO;
+
+using Xunit;
+
+namespace NetTopologySuite.Optimized.Tests
+{
+    public sealed class WKTTests
+    {
+        public static List<object[]> TestCases
+        {
+            get
+            {
+                Random rand = new Random(54321);
+                double X() => Math.Round(rand.NextDouble() * 1000, rand.Next(6));
+                double Y() => Math.Round(rand.NextDouble() * 1000, rand.Next(6));
+                string Poly(int cnt) => String.Join(", ", Enumerable.Range(0, cnt).Select(_ => $"({Seq(rand.Next(4, 15), true)})"));
+                string Seq(int cnt, bool? closed = null)
+                {
+                    bool closedVal = closed ?? rand.Next(2) == 0;
+
+                    string ender = $"{X()} {Y()}";
+                    var seq = Enumerable.Range(0, closedVal ? cnt - 2 : cnt)
+                                        .Select(_ => $"{X()} {Y()}");
+                    if (closedVal)
+                    {
+                        seq = seq.Prepend(ender).Append(ender);
+                    }
+
+                    return String.Join(", ", seq);
+                }
+
+                List<string> values = new List<string>(110);
+                for (int i = 0; i < 5; i++)
+                {
+                    values.Add($"POINT ({X()} {Y()})");
+                    values.Add($"LINESTRING ({Seq(2)})");
+                    values.Add($"LINESTRING ({Seq(rand.Next(2, 10))})");
+                    values.Add($"POLYGON ({Poly(1)})");
+                    values.Add($"POLYGON ({Poly(rand.Next(1, 10))})");
+                    values.Add($"MULTIPOINT (({X()} {Y()}))");
+                    values.Add($"MULTIPOINT ({String.Join(", ", Enumerable.Range(0, rand.Next(1, 10)).Select(_ => $"({X()} {Y()})"))})");
+                    values.Add($"MULTILINESTRING (({Seq(2)}))");
+                    values.Add($"MULTILINESTRING ({String.Join(", ", Enumerable.Range(0, rand.Next(1, 10)).Select(_ => $"({Seq(rand.Next(2, 10))})"))})");
+                    values.Add($"MULTIPOLYGON (({Poly(1)}))");
+                    values.Add($"MULTIPOLYGON ({String.Join(", ", Enumerable.Range(0, rand.Next(1, 10)).Select(_ => $"({Poly(rand.Next(1, 10))})"))})");
+                };
+
+                string[] arr = values.ToArray();
+                void ShuffleArray()
+                {
+                    // Knuth
+                    for (int i = arr.Length - 1; i > 0; i--)
+                    {
+                        int j = rand.Next(i + 1);
+                        (arr[i], arr[j]) = (arr[j], arr[i]);
+                    }
+                }
+
+                for (int i = 1; i <= arr.Length; i++)
+                {
+                    ShuffleArray();
+                    values.Add("GEOMETRYCOLLECTION (" + String.Join(", ", arr, 0, i) + ")");
+                }
+
+                return values.ConvertAll(str => new object[] { str });
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void RoundTripFromCore(string expected)
+        {
+            IGeometry geom = new WKTReader().Read(expected);
+            RawGeometry raw = new RawGeometry(geom.AsBinary());
+            string actual = OptimizedWKTWriter.Write(raw);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ NetTopologySuite.Optimized/OptimizedWKTWriter.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace NetTopologySuite.Optimized
+{
+    public static class OptimizedWKTWriter
+    {
+        private static readonly CultureInfo InvariantCulture = CultureInfo.InvariantCulture;
+
+        public static string Write(RawGeometry geometry)
+        {
+            StringBuilder builder = new StringBuilder();
+            using (StringWriter writer = new StringWriter(builder))
+            {
+                Write(geometry, writer);
+            }
+
+            return builder.ToString();
+        }
+
+        public static void Write(RawGeometry geometry, TextWriter writer)
+        {
+            switch (geometry.GeometryType)
+            {
+                case GeometryType.Point:
+                    RawPoint point = default;
+                    point.RawGeometry = geometry;
+                    WritePoint(point, writer);
+                    break;
+
+                case GeometryType.LineString:
+                    RawLineString lineString = default;
+                    lineString.RawGeometry = geometry;
+                    WriteLineString(lineString, writer);
+                    break;
+
+                case GeometryType.Polygon:
+                    RawPolygon polygon = default;
+                    polygon.RawGeometry = geometry;
+                    WritePolygon(polygon, writer);
+                    break;
+
+                case GeometryType.GeometryCollection:
+                    RawGeometryCollection collection1 = default;
+                    collection1.RawGeometry = geometry;
+                    WriteGeometryCollection(collection1, writer);
+                    break;
+
+                case GeometryType.MultiPoint:
+                    RawGeometryCollection collection2 = default;
+                    collection2.RawGeometry = geometry;
+                    WriteMultiPoint(collection2, writer);
+                    break;
+
+                case GeometryType.MultiLineString:
+                    RawGeometryCollection collection3 = default;
+                    collection3.RawGeometry = geometry;
+                    WriteMultiLineString(collection3, writer);
+                    break;
+
+                case GeometryType.MultiPolygon:
+                    RawGeometryCollection collection4 = default;
+                    collection4.RawGeometry = geometry;
+                    WriteMultiPolygon(collection4, writer);
+                    break;
+
+                default:
+                    ThrowNotSupportedExceptionForBadGeometryType();
+                    break;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void ThrowNotSupportedExceptionForBadGeometryType() =>
+            throw new NotSupportedException("Unsupported geometry type");
+
+        private static void WritePoint(RawPoint point, TextWriter writer)
+        {
+            writer.Write("POINT (");
+            writer.Write(point.X.ToString(InvariantCulture));
+            writer.Write(' ');
+            writer.Write(point.Y.ToString(InvariantCulture));
+            writer.Write(')');
+        }
+
+        private static void WriteLineString(RawLineString lineString, TextWriter writer)
+        {
+            writer.Write("LINESTRING ");
+            WriteCoordinates(lineString.Coordinates, writer);
+        }
+
+        private static void WritePolygon(RawPolygon polygon, TextWriter writer)
+        {
+            writer.Write("POLYGON (");
+
+            bool first = true;
+            foreach (var ring in polygon)
+            {
+                if (first)
+                {
+                    first = false;
+                }
+                else
+                {
+                    writer.Write(", ");
+                }
+
+                WriteCoordinates(ring, writer);
+            }
+
+            writer.Write(')');
+        }
+
+        private static void WriteGeometryCollection(RawGeometryCollection collection, TextWriter writer)
+        {
+            writer.Write("GEOMETRYCOLLECTION (");
+
+            bool first = true;
+            foreach (var geom in collection)
+            {
+                if (first)
+                {
+                    first = false;
+                }
+                else
+                {
+                    writer.Write(", ");
+                }
+
+                Write(geom, writer);
+            }
+
+            writer.Write(')');
+        }
+
+        private static void WriteMultiPoint(RawGeometryCollection collection, TextWriter writer)
+        {
+            writer.Write("MULTIPOINT (");
+
+            RawPoint pt = default;
+            bool first = true;
+            foreach (var geom in collection)
+            {
+                if (first)
+                {
+                    first = false;
+                }
+                else
+                {
+                    writer.Write(", ");
+                }
+
+                writer.Write('(');
+                pt.RawGeometry = geom;
+                writer.Write(pt.X.ToString(InvariantCulture));
+                writer.Write(' ');
+                writer.Write(pt.Y.ToString(InvariantCulture));
+                writer.Write(')');
+            }
+
+            writer.Write(')');
+        }
+
+        private static void WriteMultiLineString(RawGeometryCollection collection, TextWriter writer)
+        {
+            writer.Write("MULTILINESTRING (");
+
+            RawLineString lineString = default;
+            bool first = true;
+            foreach (var geom in collection)
+            {
+                if (first)
+                {
+                    first = false;
+                }
+                else
+                {
+                    writer.Write(", ");
+                }
+
+                lineString.RawGeometry = geom;
+                WriteCoordinates(lineString.Coordinates, writer);
+            }
+
+            writer.Write(')');
+        }
+
+        private static void WriteMultiPolygon(RawGeometryCollection collection, TextWriter writer)
+        {
+            writer.Write("MULTIPOLYGON (");
+
+            RawPolygon polygon = default;
+            bool firstPolygon = true;
+            foreach (var geom in collection)
+            {
+                if (firstPolygon)
+                {
+                    firstPolygon = false;
+                }
+                else
+                {
+                    writer.Write(", ");
+                }
+
+                polygon.RawGeometry = geom;
+                writer.Write('(');
+                bool firstRing = true;
+                foreach (var ring in polygon)
+                {
+                    if (firstRing)
+                    {
+                        firstRing = false;
+                    }
+                    else
+                    {
+                        writer.Write(", ");
+                    }
+
+                    WriteCoordinates(ring, writer);
+                }
+
+                writer.Write(')');
+            }
+
+            writer.Write(')');
+        }
+
+        private static void WriteCoordinates(RawCoordinateSequence coordinates, TextWriter writer)
+        {
+            writer.Write('(');
+
+            bool first = true;
+            foreach (var coord in coordinates)
+            {
+                if (first)
+                {
+                    first = false;
+                }
+                else
+                {
+                    writer.Write(", ");
+                }
+
+                writer.Write(coord.X.ToString(InvariantCulture));
+                writer.Write(' ');
+                writer.Write(coord.Y.ToString(InvariantCulture));
+            }
+
+            writer.Write(')');
+        }
+    }
+}
